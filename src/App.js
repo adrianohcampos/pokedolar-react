298247cefb8af05d554b27ab195e7150cb198f83
Adriano Campos <adrianohcampos@gmail.com>
2023-03-14 19:56:53 -0300

    update

--- src/App.js
+++ src/App.js
@@ -7,48 +7,35 @@ function App() {
 
 
   const [data_pd, setData] = useState(null);  
-  // const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const loadAll = async () => {
 
-      // setIsLoading(true);
+      setIsLoading(true);
 
       try {
         const dolar_info = await Api.getDolar();
-        console.log(dolar_info);
+      
         let usd = dolar_info.USD;
-        let media = Math.sum(usd.high, usd.low) / 2; 
+        let media = (Number(usd.high) +  Number(usd.high)) / 2; 
         let dolar = Math.round(media * 100) / 100;
         let real       = dolar.toFixed(2);
         let idpokemon  = dolar * 100;
-        console.log(media,dolar);
-        // const pokemom = await Api.getPokemon(idpokemon);
-        // pokemom.money = {dolar,real};
-        // console.log(pokemom);
-        // setData(pokemom);
+        
+        const pokemom = await Api.getPokemon(idpokemon);
+        let update = usd.create_date;
+        pokemom.money = {dolar,real,update};        
+        setData(pokemom);
       } catch (error) {
         console.log(error);
       } finally {
-        // setIsLoading(false);
+        setIsLoading(false);
       }
     }
 
     loadAll()
   }, [])
-
-
-//   $usd        = json_decode(file_get_contents("https://economia.awesomeapi.com.br/all/USD"));
-// $usd = $usd->USD;
-// $media      = ($usd->high + $usd->low) / 2;
-
-// $dolar      = ceil($media * 100) / 100;
-// $real       = number_format($dolar, 2, ',', '');
-// $idpokemon  = $dolar * 100;
-// $pokemon    = json_decode(file_get_contents("https://pokeapi.co/api/v2/pokemon/" . $idpokemon));
-// $update = false;
-
-// $update = $usd->create_date;
 
   return (
     <section className="pricing pt-4 pb-2">
@@ -59,22 +46,28 @@ function App() {
                     <img src="logo.png" alt="Poke Dólar" className="w-100 logo" />                      
                   </div>
                   <div className="card mb-3 mb-lg-2 shadow">
-                      <div className="card-body pokemon ">
-                        <Pokemon />
+                      <div className="card-body">
+                        {data_pd &&
+                          <Pokemon item={data_pd} />
+                        }  
+
+                        {isLoading && (
+                          <div className="loading">
+                          <img src="https://i.giphy.com/media/IQebREsGFRXmo/giphy.webp" alt="loading" className="w-75 my-5" />
+                        </div>
+                        )}                      
                       </div>
                   </div>
                   <div className="mt-2 text-center"> 
                   <span >pokedolar.com.br</span>
                       <br/> 
-                      <small> Inspirado por: <a href="https://geekvox.com.br/" rel="noreferrer" className="text-dark"
-                              target="_blank">GeekVox</a> e <a href="https://fb.com/PokeDolar/" rel="noreferrer" className="text-dark" target="_blank">Poke Dólar</a><br/> Desenvolvido por: <a href="https://acampos.com.br" className="text-dark" target="_blank" rel="noreferrer">Adriano Campos</a> </small>
+                      <small>Developed by <a href="https://acampos.com.br" className="text-dark" target="_blank" rel="noreferrer">Adriano Campos</a> </small>
                   </div>
               </div>
           </div>
       </div>
   </section>
-
   );
 }
 
-      export default App;
+export default App;
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,19 +1,33 @@
 import React from "react";
+import Helmet from "react-helmet";
 
-function Pokemon() {
+function Pokemon(item) {    
+
+    let type = []
+    let types = item.item.types;
+
+    for (let index = 0; index < types.length; index++) {
+        type.push(types[index].type.name);
+    }
+
     return (
+        
         <div>
-            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/524.png" alt="Sprite of roggenrola" className="w-75" />
-            <h2 className="text-uppercase"> $ 1 - R$ 5,24</h2>
-            <h1 className="text-uppercase"> roggenrola </h1>            
+            <Helmet>
+                <title>{`PokeDólar - #${item.item.id} ${item.item.name.toUpperCase()}`} </title>
+                <meta name="description" content="Clone Netflix in React" />
+            </Helmet>
+            <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${item.item.id}.png`} alt="Sprite of roggenrola" className="w-75" />
+            <h2> $ 1 - R$ {item.item.money.real}</h2>
+            <h1> {item.item.name.toUpperCase()} </h1>            
             <ul className="list-group text-start">
-                <li className="list-group-item"> <i className="bi bi-check-lg"></i> Name: roggenrola </li>
-                <li className="list-group-item"> <i className="bi bi-check-lg"></i> Number: 524 </li>
-                <li className="list-group-item"> <i className="bi bi-check-lg"></i> Type: rock </li>
-                <li className="list-group-item"> <i className="bi bi-check-lg"></i> Weight: 18kg </li>
-                <li className="list-group-item"> <i className="bi bi-check-lg"></i> Height: 0.4m </li>
+                <li className="list-group-item"> <i className="bi bi-check-lg"></i> Name: {item.item.name} </li>
+                <li className="list-group-item"> <i className="bi bi-check-lg"></i> Number: {item.item.id} </li>
+                <li className="list-group-item"> <i className="bi bi-check-lg"></i> Type: {type.join(', ')} </li>
+                <li className="list-group-item"> <i className="bi bi-check-lg"></i> Weight: {item.item.weight / 10}kg </li>
+                <li className="list-group-item"> <i className="bi bi-check-lg"></i> Height: {item.item.height / 10}m </li>
             </ul>
-            <div className="text-muted"><small>UPDATE 2023-03-14 10:54:28</small></div>
+            <div className="text-muted"><small>UPDATE {item.item.money.update}</small></div>
         </div>
     );
   }
